01c8b4baef9ffc18df3f4b4cd4f854260742b02b
I. Mert Inan <ibrahimmertinan@gmail.com>
2023-04-18 22:31:18 +0300

    .

--- part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -13,7 +13,19 @@ public class NumbersFromAFile {
         int lowerBound = Integer.valueOf(scanner.nextLine());
         System.out.print("Upper bound? ");
         int upperBound = Integer.valueOf(scanner.nextLine());
+        int count = 0;
+        try (Scanner scan = new Scanner(Paths.get(file))) {
+            while (scan.hasNextLine()) {
+                int number = Integer.valueOf(scan.nextLine());
+                if (number >= lowerBound && number <= upperBound) {
+                    count++;
+                }
+            }
+
+            System.out.println("Numbers: " + count);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
 
     }
-
 }
