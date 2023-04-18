12f1e8a5b1e55ae28fd4220eb10570e1b02bfdb7
I. Mert Inan <ibrahimmertinan@gmail.com>
2023-04-18 16:45:58 +0300

    .

--- part04-Part04_26.GuestListFromAFile/src/main/java/GuestListFromAFile.java
+++ part04-Part04_26.GuestListFromAFile/src/main/java/GuestListFromAFile.java
@@ -1,23 +1,28 @@
 
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class GuestListFromAFile {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Name of the file:");
         String file = scanner.nextLine();
 
         ArrayList<String> list = new ArrayList<>();
-        // implement reading the file here.
-        System.out.println("");
-
+      
         System.out.println("Enter names, an empty line quits.");
+        try (Scanner fileScanner = new Scanner(Paths.get(file))){
+        
         while (true) {
             String name = scanner.nextLine();
+            String row = fileScanner.nextLine();
+            list.add(row);
+
             if (name.isEmpty()) {
                 break;
             }
@@ -28,7 +33,7 @@ public class GuestListFromAFile {
                 System.out.println("The name is not on the list.");
             }
         }
-
+    }
         System.out.println("Thank you!");
     }
 }
